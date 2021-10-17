f93c9e7ca5e53764815510091510dda2ff10ae67
Pavani <pavanisk@gmail.com>
2021-10-17 02:19:27 -0400

    added test cases to admin suite. Created test cases to approve online accounts on customer manager and to approve cheque book requests on cheque manager

--- src/test/java/com/simplilearn/worshop/AdminTests.java
+++ src/test/java/com/simplilearn/worshop/AdminTests.java
@@ -102,6 +102,128 @@ public class AdminTests {
 			driver.quit();
 		}
 		
+		@Test 
+		public void positiveOnlineAccountAuthorizeTest() {
+			
+			System.out.println("Starting online account authorize test");
+			//create driver
+			System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+			
+			WebDriver driver = new ChromeDriver();
+			
+			sleep(2000);
+			
+			//maximize browser window
+			driver.manage().window().maximize();
+			
+			//open test page
+			String url = "http://localhost:4200/adminlogin";
+			driver.get(url);
+			System.out.println("Page is Opened");
+			
+			// enter username
+			WebElement username = driver.findElement(By.id("username"));
+			username.sendKeys("admin");
+			sleep(2000);
+			// enter password
+			WebElement password = driver.findElement(By.id("password"));
+			password.sendKeys("admin1");
+			sleep(2000);
+			// click login button
+			WebElement loginButton = driver.findElement(By.id("loginbutton"));
+			loginButton.click();
+			sleep(2000);
+			
+			// click customermanager button
+			WebElement customermanagerButton = driver.findElement(By.id("customermanager"));
+			customermanagerButton.click();
+			sleep(2000);
+			
+			// select status
+			WebElement status = driver.findElement(By.name("Authorize"));
+			status.sendKeys("block");
+			sleep(2000);
+			
+			// click submit status button
+			WebElement submitButton = driver.findElement(By.id("submitbutton"));
+			submitButton.click();
+			sleep(2000);
+			
+			//verification
+			WebElement successMessage = driver.findElement(By.id("flash"));
+			String expectedSuccessMessage = "Transaction successfully completed";
+			String actualSuccessMessage = successMessage.getText();
+					
+			Assert.assertTrue(actualSuccessMessage.contains(expectedSuccessMessage),
+					"Actual message does not contain expected message. \n Actual Message:" + actualSuccessMessage + "\n Expected Message:"  
+							+ expectedSuccessMessage);
+			sleep (2000);
+			
+			//close browser
+			driver.quit();
+		}
+		
+		@Test 
+		public void positiveChequetAuthorizeTest() {
+			
+			System.out.println("Starting cheque book request authorize test");
+			//create driver
+			System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+			
+			WebDriver driver = new ChromeDriver();
+			
+			sleep(2000);
+			
+			//maximize browser window
+			driver.manage().window().maximize();
+			
+			//open test page
+			String url = "http://localhost:4200/adminlogin";
+			driver.get(url);
+			System.out.println("Page is Opened");
+			
+			// enter username
+			WebElement username = driver.findElement(By.id("username"));
+			username.sendKeys("admin");
+			sleep(2000);
+			// enter password
+			WebElement password = driver.findElement(By.id("password"));
+			password.sendKeys("admin1");
+			sleep(2000);
+			// click login button
+			WebElement loginButton = driver.findElement(By.id("loginbutton"));
+			loginButton.click();
+			sleep(2000);
+			
+			// click chequemanager button
+			WebElement chequemanagerButton = driver.findElement(By.id("chequemanager"));
+			chequemanagerButton.click();
+			sleep(2000);
+			
+			// select status
+			WebElement status = driver.findElement(By.name("Authorize"));
+			status.sendKeys("decline");
+			sleep(2000);
+			
+			// click submit status button
+			WebElement submitButton = driver.findElement(By.id("submitbutton"));
+			submitButton.click();
+			sleep(2000);
+			
+			//verification
+			WebElement successMessage = driver.findElement(By.id("flash"));
+			String expectedSuccessMessage = "Transaction successfully completed";
+			String actualSuccessMessage = successMessage.getText();
+					
+			Assert.assertTrue(actualSuccessMessage.contains(expectedSuccessMessage),
+					"Actual message does not contain expected message. \n Actual Message:" + actualSuccessMessage + "\n Expected Message:"  
+							+ expectedSuccessMessage);
+			sleep (2000);
+			
+			//close browser
+			driver.quit();
+		}
+		
 		private void sleep(long m) {
 			try {
 			Thread.sleep(m);
